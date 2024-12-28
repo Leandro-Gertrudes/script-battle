3159aac44637cc793d067c804a31b34b574047e5
Leandro <leandropinheiro.pt@hotmail.com>
2024-12-28 03:19:19 +0000

    Add media query to index for responsive design

--- JavaScript/script-index.js
+++ JavaScript/script-index.js
@@ -1,7 +1,10 @@
 // index script
 function start() {
-   const userName = document.querySelector("#user-name").value;
+    const userName = document.querySelector("#user-name").value;
     const selectedClass = document.querySelector('input[name="class"]:checked');
+
+
+
     if (selectedClass && userName) {
         sessionStorage.setItem('userName', userName);
         sessionStorage.setItem('classSoldier', selectedClass.id);
@@ -9,6 +12,18 @@ function start() {
     } else {
         alert("Por favor, escolha um nome e uma classe.");
         return; 
+    }
+}
+function instructions() {
+    const home = document.querySelector("#contain-index");
+    const infos = document.querySelector("#infos");
+
+    if (infos.style.display === "inline-block") {
+        home.style.display = "inline-block";
+        infos.style.display = "none";
+    } else {
+        home.style.display = "none";
+        infos.style.display = "inline-block";
     }
 }
 
